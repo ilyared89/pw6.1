ca936630023370ce4b15582fada6e8347e82c105
sniper <mnml.sniper@gmail.com>
2026-02-24 22:10:16 +0300

    2 lesson

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
@@ -39,7 +40,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -49,7 +50,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
---
+++ tests/2.spec.js
@@ -0,0 +1,60 @@
+import { test, expect } from '@playwright/test';
+
+test('Поиск ролевых селекторов', async ({ page }) => {
+  await page.goto('file:///Users/sniper/pw6/burger-order.html');
+  await page.getByRole('textbox', { name: 'Имя клиента:' }).fill('Sniper');
+  await page.getByRole('combobox', { name: 'Тип бургера:' }).selectOption('cheeseburger');
+  await page.getByRole('radio', { name: 'Большой' }).click();
+  await page.getByRole('checkbox', { name: 'Горчица' }).click();
+// todo не срабатывает  await page.getByRole('checkbox', { name: 'Да' }).click();
+await page.getByRole('button', { name: '+' }).click();
+await page.getByRole('radio', { name: 'Картой онлайн' }).click();
+await page.getByRole('button', { name: 'Заказать бургер' }).click();
+await expect(page.getByRole('heading', { name: '✅ Заказ принят!'})).toBeVisible();
+
+//todo
+// await expect(page.getByRole('paragraph', { name: '/Спасибо за заказ, Sniper!/'})).toBeVisible();
+
+
+});
+
+test('Поиск селекторов по классу', async ({ page }) => {
+    await page.goto('file:///Users/sniper/pw6/burger-order.html');
+    await page.locator('.order-form').locator('.form-group').locator('input').first().fill('Sniper');
+    await page.locator('.order-form').locator('select').first().selectOption('cheeseburger');
+    await page.locator('.radio-group').filter({hasText: 'Большой'}).click();
+    await page.locator('.checkbox-group').filter({hasText: 'Горчица'}).click();
+    await page.locator('.switch-label').click();
+    await page.locator('.counter-increase').click();
+    await page.locator('.radio-group').filter({hasText: 'Картой онлайн'}).click();
+    await page.locator('.btn-primary').first().click();
+// todo
+    await expect(page.getByRole('heading', { name: '✅ Заказ принят!'})).toBeVisible();
+
+  });
+
+  test('Поиск селекторов по ID', async ({ page }) => {
+    await page.goto('file:///Users/sniper/pw6/burger-order.html');
+    await page.locator('#customerName').fill('Sniper');
+    await page.locator('#burgerType').selectOption('cheeseburger');
+    await page.locator('#quantity').fill('2');
+
+    await page.getByRole('button', { name: 'Заказать бургер' }).click();
+
+// todo
+    await expect(page.getByRole('heading', { name: '✅ Заказ принят!'})).toBeVisible();
+
+  });
+  test.only('Поиск селекторов по атрибуту и значению', async ({ page }) => {
+    await page.goto('file:///Users/sniper/pw6/burger-order.html');
+    await page.locator('[name="customerName"]').fill('Sniper');
+
+    await page.locator('[name="burgerType"]').selectOption('cheeseburger');
+    await page.locator('#quantity').fill('2');
+
+    await page.getByRole('button', { name: 'Заказать бургер' }).click();
+
+// todo
+    await expect(page.getByRole('heading', { name: '✅ Заказ принят!'})).toBeVisible();
+
+  });
